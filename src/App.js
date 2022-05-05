dbb0eb223f3233831dc788d022e3d71d9d85414f
Manh Hieu <hieuluis96@gmail.com>
2022-05-05 11:19:27 +0700

    add project url

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { isAndroid, isIOS } from 'react-device-detect';
+import './App.css';
+import Logo from "./Logo.jpg";
+
+
+
+const PLAY_STORE_LINK = "https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.vnfite.vnfiteapp";
+const APP_STORE_LINK = "https://apps.apple.com/vn/app/vnfite-fintech-p2p-lending/id1610082008?l=vi"
+function App() {
+  React.useEffect(() => {
+    if (isAndroid) {
+      window.location.href =
+        PLAY_STORE_LINK;
+    } else if (isIOS) {
+      window.location.href =
+        APP_STORE_LINK;
+    } else {
+      window.location.href =
+        "https://vnfite.com.vn/";
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <img src={Logo} alt='Logo' />
+    </div>
+  );
+}
+
+export default App;
